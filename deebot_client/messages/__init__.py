72cf243e08f3a190c1cb35ec0e68d02f06124e2f
Robert Resch <robert@resch.dev>
2022-01-27 21:18:07 +0100

    add onBattery

--- deebot_client/messages/__init__.py
+++ deebot_client/messages/__init__.py
@@ -1,23 +1,17 @@
 """Messages module."""
 
 
-from typing import Dict, Type
-
-from ..commands import COMMANDS_WITH_HANDLING
 from ..message import Message
 from ..messages.stats import ReportStats
+from .battery import OnBattery
 
 # fmt: off
 # ordered by file asc
 _MESSAGES: list[type[Message]] = [
+    OnBattery,
+
     ReportStats
 ]
 # fmt: on
 
-MESSAGES: dict[str, type[Message]] = {
-    message.name: message
-    for message in (
-        _MESSAGES
-        + [cmd for cmd in COMMANDS_WITH_HANDLING.values() if issubclass(cmd, Message)]
-    )
-}
+MESSAGES: dict[str, type[Message]] = {message.name: message for message in _MESSAGES}
