854f3d6816470eda13e3d14cba6c733f99b9dc61
Robert Resch <robert@resch.dev>
2023-05-25 16:49:01 +0000

    add first authenticator test

---
+++ tests/test_authentication.py
@@ -0,0 +1,47 @@
+import asyncio
+import time
+from unittest.mock import AsyncMock, patch
+
+import pytest
+
+from deebot_client.authentication import Authenticator
+from deebot_client.models import Configuration, Credentials
+
+
+async def test_authenticator_authenticate(config: Configuration) -> None:
+    on_changed_called = asyncio.Event()
+
+    async def on_changed(_: Credentials) -> None:
+        if on_changed_called.is_set():
+            pytest.fail("Event was already set")
+        on_changed_called.set()
+
+    with patch("deebot_client.authentication._AuthClient", spec_set=True) as api_client:
+        login_mock: AsyncMock = api_client.return_value.login
+        login_mock.return_value = Credentials(
+            "token", "user_id", int(time.time() + 123456789)
+        )
+        authenticator = Authenticator(config, "test", "test")
+
+        unsub = authenticator.subscribe(on_changed)
+
+        assert (await authenticator.authenticate()) == login_mock.return_value
+        login_mock.assert_awaited_once()
+        async with asyncio.timeout(0.1):
+            await on_changed_called.wait()
+            on_changed_called.clear()
+
+        login_mock.reset_mock()
+
+        # reauthenticate but this time we can used the cached credentials
+        assert (await authenticator.authenticate()) == login_mock.return_value
+        login_mock.assert_not_called()
+        assert not on_changed_called.is_set()
+
+        # Unsubscribe from authenticator
+        unsub()
+
+        # reauthenticate with force=True should call again the api
+        assert (await authenticator.authenticate(True)) == login_mock.return_value
+        login_mock.assert_awaited_once()
+        assert not on_changed_called.is_set()
