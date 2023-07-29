0034c52f8e294ffc8f1d8ea30d9aea1e1b77b8c5
Robert Resch <robert@resch.dev>
2023-07-29 12:43:55 +0300

    change speed type of FanSpeedEvent (#281)
    * change speed type of FanSpeedEvent
    
    * adjust fan speed tests

---
+++ deebot_client/events/fan_speed.py
@@ -0,0 +1,24 @@
+"""Fan speed event module."""
+
+
+from dataclasses import dataclass
+
+from ..util import DisplayNameIntEnum
+from .base import Event
+
+
+class FanSpeedLevel(DisplayNameIntEnum):
+    """Enum class for all possible fan speed levels."""
+
+    # Values should be sort from low to high on their meanings
+    QUIET = 1000
+    NORMAL = 0
+    MAX = 1
+    MAX_PLUS = 2
+
+
+@dataclass(frozen=True)
+class FanSpeedEvent(Event):
+    """Fan speed event representation."""
+
+    speed: FanSpeedLevel
