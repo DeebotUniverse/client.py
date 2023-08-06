c82905777a85fc4393a62fce4e982a9f02bb1184
Sander van Kasteel <info@sandervankasteel.nl>
2023-08-06 22:55:38 +0200

    Added Charge command for XML based devices

--- deebot_client/commands/charge.py
+++ deebot_client/commands/charge.py
@@ -1,10 +1,12 @@
 """Charge commands."""
 from typing import Any
 
+from ..authentication import Authenticator
+from ..command import CommandResult
 from ..events import StateEvent
 from ..logging_filter import get_logger
 from ..message import HandlingResult
-from ..models import VacuumState
+from ..models import VacuumState, DeviceInfo
 from .common import EventBus, ExecuteCommand
 from .const import CODE
 
@@ -16,8 +18,7 @@ class Charge(ExecuteCommand):
 
     name = "charge"
 
-    def __init__(self) -> None:
-        super().__init__({"act": "go"})
+    xml_name = "Charge"
 
     @classmethod
     def _handle_body(cls, event_bus: EventBus, body: dict[str, Any]) -> HandlingResult:
@@ -36,3 +37,15 @@ class Charge(ExecuteCommand):
             return HandlingResult.success()
 
         return super()._handle_body(event_bus, body)
+
+    async def _execute(
+            self, authenticator: Authenticator, device_info: DeviceInfo, event_bus: EventBus
+    ) -> CommandResult:
+
+        if device_info.uses_xml_protocol:
+            self._args["type"] = "go"
+
+        if not device_info.uses_xml_protocol:
+            self._args["act"] = "go"
+
+        return await super()._execute(authenticator, device_info, event_bus)
