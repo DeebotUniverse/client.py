e336209170e8d1a50fb5b739da7d8481dd863b97
Sander van Kasteel <info@sandervankasteel.nl>
2023-08-06 22:54:54 +0200

    Added xml_name property for XML based devices

--- deebot_client/commands/battery.py
+++ deebot_client/commands/battery.py
@@ -8,6 +8,8 @@ class GetBattery(OnBattery, NoArgsCommand):
 
     name = "getBattery"
 
+    xml_name = "GetBatteryInfo"
+
     def __init__(self, is_available_check: bool = False) -> None:
         super().__init__()
         self._is_available_check = is_available_check
