f111426e6751bb41166829c63e1177af979f00a2
Sander van Kasteel <info@sandervankasteel.nl>
2023-08-07 23:22:12 +0200

    Added broken teset for getBattery over MQTT and XML

--- tests/messages/test_battery.py
+++ tests/messages/test_battery.py
@@ -20,3 +20,22 @@ def test_getBattery(percentage: int) -> None:
     }
 
     assert_message(OnBattery, data, BatteryEvent(percentage))
+
+
+@pytest.mark.parametrize("percentage", [0, 49, 100])
+def test_getBattery_xml(percentage: int) -> None:
+    data = {
+        "header": {
+            "pri": 1,
+            "tzm": 480,
+            "ts": "1304637391896",
+            "ver": "0.0.1",
+            "fwVer": "1.8.2",
+            "hwVer": "0.1.1",
+        },
+        "body": {
+            "resp": "<ctl ret='ok'><battery power='{}'/></ctl>".format(percentage)
+        },
+    }
+
+    assert_message(OnBattery, data, BatteryEvent(percentage))
