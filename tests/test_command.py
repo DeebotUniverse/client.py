781d86684d5ce86a4017fb746be9ba4b74f9372f
Robert Resch <robert@resch.dev>
2023-10-15 08:22:22 +0000

    add test for addiotnal fields

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,14 +1,32 @@
 from typing import Any
 
 import pytest
+from testfixtures import LogCapture
 
-from deebot_client.command import CommandMqttP2P, InitParam
+from deebot_client.command import CommandMqttP2P, CommandResult, InitParam
+from deebot_client.const import DataType
+from deebot_client.events.event_bus import EventBus
 from deebot_client.exceptions import DeebotError
 
 
 class _TestCommand(CommandMqttP2P):
     name = "TestCommand"
-    _mqtt_params = {"field": InitParam(int)}
+    data_type = DataType.JSON
+    _mqtt_params = {"field": InitParam(int), "remove": None}
+
+    def __init__(self, field: int) -> None:
+        pass
+
+    def handle_mqtt_p2p(self, event_bus: EventBus, response: dict[str, Any]) -> None:
+        pass
+
+    def _get_payload(self) -> dict[str, Any] | list | str:
+        return {}
+
+    def _handle_response(
+        self, event_bus: EventBus, response: dict[str, Any]
+    ) -> CommandResult:
+        return CommandResult.analyse()
 
 
 def test_CommandMqttP2P_no_mqtt_params() -> None:
@@ -31,3 +49,16 @@ def test_CommandMqttP2P_create_from_mqtt_error(
 ) -> None:
     with pytest.raises(DeebotError, match=expected):
         _TestCommand.create_from_mqtt(data)
+
+
+def test_CommandMqttP2P_create_from_mqtt_additional_fields() -> None:
+    with LogCapture() as log:
+        _TestCommand.create_from_mqtt({"field": 0, "remove": "bla", "additional": 1})
+
+        log.check_present(
+            (
+                "deebot_client.command",
+                "DEBUG",
+                "Following data will be ignored: {'additional': 1}",
+            )
+        )
