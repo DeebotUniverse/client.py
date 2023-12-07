ca0121b81ae0dff2056af7ef7457cb73a291caa5
mvladislav <dev@mvladislav.online>
2023-12-07 02:03:42 +0100

    refactor: command auto empty improve test case - remove 'noqa: FBT003' - extend with fail test case

--- tests/commands/json/test_auto_empty.py
+++ tests/commands/json/test_auto_empty.py
@@ -22,27 +22,27 @@ def test_WorkMode_unique() -> None:
     [
         (
             {"enable": 1, "frequency": "10"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_10),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_10),
         ),
         (
             {"enable": 1, "frequency": "15"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_15),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_15),
         ),
         (
             {"enable": 1, "frequency": "25"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_25),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_25),
         ),
         (
             {"enable": 0, "frequency": "25"},
-            AutoEmptyModeEvent(False, AutoEmptyMode.MODE_25),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=False, mode=AutoEmptyMode.MODE_25),
         ),
         (
             {"enable": 1, "frequency": "auto"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_AUTO),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_AUTO),
         ),
         (
             {"enable": 1, "frequency": "smart"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_SMART),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_SMART),
         ),
     ],
 )
@@ -57,31 +57,33 @@ async def test_GetAutoEmpty(json: dict[str, Any], expected: AutoEmptyModeEvent) 
         (
             (True, AutoEmptyMode.MODE_10),
             {"enable": 1, "frequency": "10"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_10),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_10),
         ),
         (
             (True, "mode_smart"),
             {"enable": 1, "frequency": "smart"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_SMART),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_SMART),
         ),
+        # NOTE: this test is also working, as 'enable' will set auto to 'true' if not provided
+        # as 'enable' is required when set a 'frequency'
         (
             (None, AutoEmptyMode.MODE_25),
             {"enable": 1, "frequency": "25"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_25),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_25),
         ),
         # NOTE: it should be possible to only send 'True' for turn on without 'frequency',
         # but not sure how to implement the test correct
         (
             (True, AutoEmptyMode.MODE_AUTO),
             {"enable": 1, "frequency": "auto"},
-            AutoEmptyModeEvent(True, AutoEmptyMode.MODE_AUTO),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=True, mode=AutoEmptyMode.MODE_AUTO),
         ),
         # NOTE: it should be possible to only send 'False' for turn off without 'frequency',
         # but not sure how to implement the test correct
         (
             (False, AutoEmptyMode.MODE_AUTO),
             {"enable": 0, "frequency": "auto"},
-            AutoEmptyModeEvent(False, AutoEmptyMode.MODE_AUTO),  # noqa: FBT003
+            AutoEmptyModeEvent(enable=False, mode=AutoEmptyMode.MODE_AUTO),
         ),
     ],
 )
@@ -99,3 +101,35 @@ async def test_SetAutoEmpty(
         command = SetAutoEmpty(value[0], value[1])
 
     await assert_set_command(command, args, expected)
+
+
+@pytest.mark.parametrize(
+    ("value", "args", "expected"),
+    [
+        (
+            (None, AutoEmptyMode.MODE_AUTO),
+            {"enable": 0, "frequency": "auto"},
+            AutoEmptyModeEvent(enable=False, mode=AutoEmptyMode.MODE_AUTO),
+        ),
+        (
+            (None, None),
+            {"enable": 0, "frequency": "auto"},
+            AutoEmptyModeEvent(enable=False, mode=AutoEmptyMode.MODE_AUTO),
+        ),
+    ],
+)
+async def test_SetAutoEmptyFail(
+    value: tuple[bool | None, AutoEmptyMode | str | None],
+    args: dict[str, Any],
+    expected: AutoEmptyModeEvent,
+) -> None:
+    command = SetAutoEmpty()
+    if value[0] is None and value[1] is not None:
+        command = SetAutoEmpty(frequency=value[1])
+    elif value[1] is None and value[0] is not None:
+        command = SetAutoEmpty(enable=value[0])
+    elif value[0] is not None and value[1] is not None:
+        command = SetAutoEmpty(value[0], value[1])
+
+    with pytest.raises(AssertionError):
+        await assert_set_command(command, args, expected)
