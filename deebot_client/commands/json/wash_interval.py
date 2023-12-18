c82254cad10e74b07a8e06153c1c9ca2eba44b28
NEVdataDyne <82730421+NEVdataDyne@users.noreply.github.com>
2023-12-18 17:16:50 +0100

    Update wash_interval.py - Wash interval correction

--- deebot_client/commands/json/wash_interval.py
+++ deebot_client/commands/json/wash_interval.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from deebot_client.command import InitParam
 from deebot_client.event_bus import EventBus
-from deebot_client.events.pads_cleaning_interval import PadsCleaningIntervalEvent
+from deebot_client.events.wash_interval import WashIntervalEvent
 from deebot_client.message import HandlingResult, MessageBodyDataDict
 
 from .common import CommandWithMessageHandling, SetCommand
