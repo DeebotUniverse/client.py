cf4ff86535eaed109e3186b7391a8c860e5db5c3
NEVdataDyne <82730421+NEVdataDyne@users.noreply.github.com>
2023-12-18 16:20:12 +0100

    Update wash_interval.py - Wash interval

--- deebot_client/events/wash_interval.py
+++ deebot_client/events/wash_interval.py
@@ -1,1 +1,11 @@
+"""Cleaning pads interval event module."""
+from dataclasses import dataclass
 
+from .base import Event
+
+
+@dataclass(frozen=True)
+class WashIntervalEvent(Event):
+    """Cleaning pads interval event representation."""
+
+    interval: int
