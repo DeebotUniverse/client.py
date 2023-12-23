a7de74f3d65e5b532ec8fb49610826bbb3535d02
Robert Resch <robert@resch.dev>
2023-12-23 19:15:12 +0000

    fix test

--- tests/test_map.py
+++ tests/test_map.py
@@ -11,13 +11,19 @@ from deebot_client.events.map import (
     Position,
     PositionType,
 )
-from deebot_client.map import Map, MapData, _calc_point
+from deebot_client.map import (
+    AxisManipulation,
+    Map,
+    MapData,
+    MapManipulation,
+    Point,
+    _calc_point,
+)
 from deebot_client.models import Room
 
 _test_calc_point_data = [
-    (0, 10, None, (0.0, 10.0)),
-    (10, 100, (100, 0, 200, 50), (200.0, 50.0)),
-    (10, 100, (0, 0, 1000, 1000), (400.2, 402.0)),
+    (10, 100, (100, 0, 200, 50), Point(100.0, 0.0)),
+    (10, 100, (0, 0, 1000, 1000), Point(400.2, 598.0)),
 ]
 
 
@@ -25,10 +31,22 @@ _test_calc_point_data = [
 def test_calc_point(
     x: int,
     y: int,
-    image_box: tuple[int, int, int, int] | None,
-    expected: tuple[float, float],
+    image_box: tuple[int, int, int, int],
+    expected: Point,
 ) -> None:
-    result = _calc_point(x, y, image_box)
+    manipulation = MapManipulation(
+        AxisManipulation(
+            map_shift=image_box[0],
+            svg_max=image_box[2] - image_box[0],
+            _transform=lambda _, y: y,
+        ),
+        AxisManipulation(
+            map_shift=image_box[1],
+            svg_max=image_box[3] - image_box[1],
+            _transform=lambda x, y: 2 * x - y,
+        ),
+    )
+    result = _calc_point(x, y, manipulation)
     assert result == expected
 
 
