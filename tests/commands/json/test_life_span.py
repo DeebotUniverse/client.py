8293437702c7752fdacc37d279227958142da482
mrbungle64 <mrb1232@posteo.de>
2023-12-28 22:46:19 +0100

    Add several LifeSpan components
    ROUND_MOP = "Mopping pad" (e.g. X1 series)
    AIR_FRESHENER = "Air Freshener" (e.g. T9 series, Airbot Z1)
    UV_SANITIZER = "UV Sanitizer Module" (Airbot Z1)
    HUMIDIFY = "Humidification Filter" (Airbot Z1)
    HUMIDIFY_MAINTENANCE = "Humidification Module Maintenance" (Airbot Z1)

--- tests/commands/json/test_life_span.py
+++ tests/commands/json/test_life_span.py
@@ -20,6 +20,11 @@ from . import assert_command, assert_execute_command
                     LifeSpan.FILTER,
                     LifeSpan.SIDE_BRUSH,
                     LifeSpan.UNIT_CARE,
+                    LifeSpan.ROUND_MOP,
+                    LifeSpan.AIR_FRESHENER,
+                    LifeSpan.UV_SANITIZER,
+                    LifeSpan.HUMIDIFY,
+                    LifeSpan.HUMIDIFY_MAINTENANCE,
                 }
             ),
             get_request_json(
@@ -29,6 +34,11 @@ from . import assert_command, assert_execute_command
                         {"type": "heap", "left": 7179, "total": 7200},
                         {"type": "sideBrush", "left": 8977, "total": 9000},
                         {"type": "unitCare", "left": 265, "total": 1800},
+                        {"type": 'roundMop', "left": 6820, "total": 9000},
+                        {"type": 'dModule', "left": 17537, "total": 18000},
+                        {"type": 'uv', "left": 898586, "total": 900000},
+                        {"type": 'humidify', "left": 191547, "total": 194400},
+                        {"type": 'wbCare', "left": 22260, "total": 43200}
                     ]
                 )
             ),
@@ -37,6 +47,11 @@ from . import assert_command, assert_execute_command
                 LifeSpanEvent(LifeSpan.FILTER, 99.71, 7179),
                 LifeSpanEvent(LifeSpan.SIDE_BRUSH, 99.74, 8977),
                 LifeSpanEvent(LifeSpan.UNIT_CARE, 14.72, 265),
+                LifeSpanEvent(LifeSpan.ROUND_MOP, 75.78, 6820),
+                LifeSpanEvent(LifeSpan.AIR_FRESHENER, 97.43, 17537),
+                LifeSpanEvent(LifeSpan.UV_SANITIZER, 99.84, 898586),
+                LifeSpanEvent(LifeSpan.HUMIDIFY, 98.53, 191547),
+                LifeSpanEvent(LifeSpan.HUMIDIFY_MAINTENANCE, 51.53, 22260),
             ],
         ),
         (
@@ -66,6 +81,41 @@ from . import assert_command, assert_execute_command
                 get_success_body([{"type": "unitCare", "left": 265, "total": 1800}])
             ),
             [LifeSpanEvent(LifeSpan.UNIT_CARE, 14.72, 265)],
+        ),
+        (
+            GetLifeSpan({LifeSpan.ROUND_MOP}),
+            get_request_json(
+                get_success_body([{"type": "roundMop", "left": 6820, "total": 9000}])
+            ),
+            [LifeSpanEvent(LifeSpan.ROUND_MOP, 75.78, 6820)],
+        ),
+        (
+            GetLifeSpan({LifeSpan.AIR_FRESHENER}),
+            get_request_json(
+                get_success_body([{"type": "unitCare", "left": 17537, "total": 18000}])
+            ),
+            [LifeSpanEvent(LifeSpan.AIR_FRESHENER, 97.43, 17537)],
+        ),
+        (
+            GetLifeSpan({LifeSpan.UV_SANITIZER}),
+            get_request_json(
+                get_success_body([{"type": "uv", "left": 898586, "total": 900000}])
+            ),
+            [LifeSpanEvent(LifeSpan.UV_SANITIZER, 99.84, 898586)],
+        ),
+        (
+            GetLifeSpan({LifeSpan.HUMIDIFY}),
+            get_request_json(
+                get_success_body([{"type": "humidify", "left": 191547, "total": 194400}])
+            ),
+            [LifeSpanEvent(LifeSpan.HUMIDIFY, 98.53, 191547)],
+        ),
+        (
+            GetLifeSpan({LifeSpan.HUMIDIFY_MAINTENANCE}),
+            get_request_json(
+                get_success_body([{"type": "wbCare", "left": 22260, "total": 43200}])
+            ),
+            [LifeSpanEvent(LifeSpan.HUMIDIFY_MAINTENANCE, 51.53, 22260)],
         ),
     ],
 )
