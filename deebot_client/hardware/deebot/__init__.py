4008cf6ba8dc162a6fdb810b9a9c93676f98bfbb
Robert Resch <robert@resch.dev>
2024-01-27 20:46:25 +0100

    Update no capabilities log entry (#404)

--- deebot_client/hardware/deebot/__init__.py
+++ deebot_client/hardware/deebot/__init__.py
@@ -35,5 +35,8 @@ def get_static_device_info(class_: str) -> StaticDeviceInfo:
         _LOGGER.debug("Capabilities found for %s", class_)
         return device
 
-    _LOGGER.warning("No capabilities found for %s. Using fallback.", class_)
+    _LOGGER.info(
+        "No capabilities found for %s, therefore not all features are available. trying to use fallback...",
+        class_,
+    )
     return DEVICES[FALLBACK]
