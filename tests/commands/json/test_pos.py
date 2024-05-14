4c84f71cbb5d1fd16a2342f0bee3c0abe4e5d1d7
Lorenzo Monaco <1611929+lnx85@users.noreply.github.com>
2024-05-14 21:50:51 +0200

    Update test_pos.py

--- tests/commands/json/test_pos.py
+++ tests/commands/json/test_pos.py
@@ -10,7 +10,7 @@ from . import assert_command
 async def test_GetPos() -> None:
     json = get_request_json(
         get_success_body(
-            {"chargePos": {"x":5, "y":9}, "deebotPos": {"x": 1, "y":5, "a":85}}
+            {"chargePos": {"x": 5, "y": 9}, "deebotPos": {"x": 1, "y": 5, "a": 85}}
         )
     )
     expected_event = PositionsEvent(
