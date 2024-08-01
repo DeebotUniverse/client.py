fd394ac40ae9ed9823594983abd787f8c48c1ee9
Robert Resch <robert@resch.dev>
2024-08-01 07:00:53 +0000

    Bump aiohttp to 3.10

--- deebot_client/exceptions.py
+++ deebot_client/exceptions.py
@@ -2,6 +2,11 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from aiohttp import ClientTimeout
+
 
 class DeebotError(Exception):
     """Deebot error."""
@@ -22,7 +27,7 @@ class ApiError(DeebotError):
 class ApiTimeoutError(ApiError):
     """Api timeout error."""
 
-    def __init__(self, path: str, timeout: int, *args: object) -> None:
+    def __init__(self, path: str, timeout: ClientTimeout, *args: object) -> None:
         super().__init__(f"Timeout ({timeout}) reached on path: {path}", *args)
 
 
