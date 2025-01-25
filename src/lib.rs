a45dbc314f15bd48d9ea554f11b486e284cbde77
Robert Resch <robert@resch.dev>
2025-01-25 21:43:57 +0000

    Improve submodule registration in rust

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,31 @@ mod util;
 
 /// Deebot client written in Rust
 #[pymodule]
-fn rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    register_child_module(m, "map", map::init_module)?;
-    register_child_module(m, "util", util::init_module)?;
+fn rs(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    register_submodule(py, m, map::init_module)?;
+    register_submodule(py, m, util::init_module)?;
     Ok(())
 }
 
-fn register_child_module(
+fn register_submodule<F>(
+    py: Python<'_>,
     parent_module: &Bound<'_, PyModule>,
-    name: &str,
-    func: fn(&Bound<'_, PyModule>) -> PyResult<()>,
-) -> PyResult<()> {
-    let child_module = PyModule::new(parent_module.py(), name)?;
-    func(&child_module)?;
+    submodule: F,
+) -> PyResult<()>
+where
+    F: Fn(&Bound<'_, PyModule>) -> PyResult<()>,
+{
+    let parts: Vec<&str> = std::any::type_name::<F>().split("::").collect();
+    let module_name = parts[parts.len() - 2];
+
+    let child_module = PyModule::new(parent_module.py(), module_name)?;
+    submodule(&child_module)?;
 
     // https://github.com/PyO3/pyo3/issues/1517#issuecomment-808664021
     // https://github.com/PyO3/pyo3/issues/759
-    let _ = Python::with_gil(|py| {
-        py.import("sys")?
-            .getattr("modules")?
-            .set_item(&format!("deebot_client.rs.{}", name), &child_module)
-    });
+    PyModule::import(py, "sys")?
+        .getattr("modules")?
+        .set_item(&format!("deebot_client.rs.{}", module_name), &child_module)?;
 
     parent_module.add_submodule(&child_module)
 }
