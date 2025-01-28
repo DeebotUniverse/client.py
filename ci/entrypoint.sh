0bfd8d44031320c4abf3365cbf383fb769379b18
Robert Resch <robert@resch.dev>
2025-01-28 23:26:04 +0000

    fix variable

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -8,6 +8,6 @@ rm -rf deebot_client
 
 uv pip install --no-progress --system --force-reinstall dist/deebot_client-*.whl
 # Disable tests, which require docker for now
-site-packages=$(python -c "import site; print(site.getsitepackages()[0])")
-echo "site-packages: ${site-packages}"
-pytest tests --cov=${site-packages}/deebot_client --cov-report=xml --junitxml=junit.xml -o junit_family=legacy -v -m "not docker"
+site_packages=$(python -c "import site; print(site.getsitepackages()[0])")
+echo "site-packages: ${site_packages}"
+pytest tests --cov=${site_packages}/deebot_client --cov-report=xml --junitxml=junit.xml -o junit_family=legacy -v -m "not docker"
