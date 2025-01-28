ae20eff1dcc00a43b53acf5b1198396ff8855324
Robert Resch <robert@resch.dev>
2025-01-28 18:26:43 +0000

    Test

---
+++ ci/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -euxo pipefail
+
+uv pip install --no-progress --system --force-reinstall dist/deebot_client-*.whl
+# Disable tests, which require docker for now
+pytest tests --cov=./ --cov-report=xml --junitxml=junit.xml -o junit_family=legacy -v -m "not docker"
