c5d4ecac8155078c268151a14c544514886be749
Robert Resch <robert@resch.dev>
2025-01-28 19:24:42 +0000

    Fix by removing source files

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -2,7 +2,10 @@
 set -euxo pipefail
 
 cd /github/workspace
-ls -al *
+
+# Remove sources otherwise pytest will complain about missing rust files
+rm -rf deebot_client
+
 uv pip install --no-progress --system --force-reinstall dist/deebot_client-*.whl
 # Disable tests, which require docker for now
 pytest tests --cov=./ --cov-report=xml --junitxml=junit.xml -o junit_family=legacy -v -m "not docker"
