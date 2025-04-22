1f0b9ad7f942ff8dc0387389b95a53fe3ba8aa6e
Giovanni Condello <giovanni.condello@coderit.it>
2025-04-22 13:05:11 +0200

    Integrate PR feedback

--- deebot_client/commands/xml/clean_logs.py
+++ deebot_client/commands/xml/clean_logs.py
@@ -2,10 +2,12 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from enum import StrEnum, unique
+from typing import TYPE_CHECKING, Self
 
 from deebot_client.command import CommandResult
 from deebot_client.events import (
+    CleanJobStatus,
     CleanLogEntry,
     CleanLogEvent,
 )
@@ -13,7 +15,6 @@ from deebot_client.logging_filter import get_logger
 from deebot_client.message import HandlingResult
 
 from .common import XmlCommandWithMessageHandling
-from .enum import XmlStopReason
 
 if TYPE_CHECKING:
     from xml.etree.ElementTree import Element
@@ -21,6 +22,40 @@ if TYPE_CHECKING:
     from deebot_client.event_bus import EventBus
 
 _LOGGER = get_logger(__name__)
+
+
+@unique
+class XmlStopReason(StrEnum):
+    """XML Reasons why cleaning has been stopped."""
+
+    clean_job_status: CleanJobStatus
+
+    def __new__(cls, value: str, clean_job_status: CleanJobStatus) -> Self:
+        """Create new XmlStopReason."""
+        obj = str.__new__(cls, value)
+        obj._value_ = value
+        obj.clean_job_status = clean_job_status
+        return obj
+
+    FINISHED = "s", CleanJobStatus.FINISHED
+    BATTERY_LOW = "r", CleanJobStatus.FINISHED_WITH_WARNINGS
+    STOPPED_BY_APP = "a", CleanJobStatus.MANUALLY_STOPPED
+    STOPPED_BY_REMOTE_CONTROL = "i", CleanJobStatus.MANUALLY_STOPPED
+    STOPPED_BY_BUTTON = "b", CleanJobStatus.MANUALLY_STOPPED
+    STOPPED_BY_WARNING = "w", CleanJobStatus.FINISHED_WITH_WARNINGS
+    STOPPED_BY_NO_DISTURB = "f", CleanJobStatus.FINISHED_WITH_WARNINGS
+    STOPPED_BY_CLEARMAP = "m", CleanJobStatus.FINISHED_WITH_WARNINGS
+    STOPPED_BY_NO_PATH = "n", CleanJobStatus.FINISHED_WITH_WARNINGS
+    STOPPED_BY_NOT_IN_MAP = "u", CleanJobStatus.FINISHED_WITH_WARNINGS
+    STOPPED_BY_VIRTUAL_WALL = "v", CleanJobStatus.FINISHED_WITH_WARNINGS
+
+    @classmethod
+    def from_value(cls, value: str) -> XmlStopReason:
+        """Fetch the right enum member given its string value."""
+        for elem in cls.__members__.values():
+            if elem.value == value:
+                return elem
+        raise ValueError(value)
 
 
 class GetCleanLogs(XmlCommandWithMessageHandling):
