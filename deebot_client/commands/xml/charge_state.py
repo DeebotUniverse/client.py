79fb57ac1473f18b77609299af65bc2284bbf26e
Giovanni Condello <giovanni.condello@coderit.it>
2025-04-24 13:09:12 +0200

    Integrate PR feedback

--- deebot_client/commands/xml/charge_state.py
+++ deebot_client/commands/xml/charge_state.py
@@ -4,9 +4,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from deebot_client.events import StateEvent
 from deebot_client.message import HandlingResult
-from deebot_client.models import State
+from deebot_client.messages.xml import ChargeState
 
 from .common import XmlCommandWithMessageHandling
 
@@ -16,34 +15,7 @@ if TYPE_CHECKING:
     from deebot_client.event_bus import EventBus
 
 
-class ChargeStateParser:
-    """Support class for producing ChargeState events."""
-
-    @classmethod
-    def _parse_xml(cls, event_bus: EventBus, xml: Element) -> HandlingResult:
-        """Handle xml message and notify the correct event subscribers.
-
-        :return: A message response
-        """
-        if (charge := xml.find("charge")) is not None and (
-            charge_type := charge.attrib["type"]
-        ) is not None:
-            match charge_type.lower():
-                case "slotcharging" | "slot_charging" | "wirecharging":
-                    status = State.DOCKED
-                case "idle":
-                    status = State.IDLE
-                case "going":
-                    status = State.RETURNING
-                case _:
-                    status = State.ERROR
-            event_bus.notify(StateEvent(status))
-            return HandlingResult.success()
-
-        return HandlingResult.analyse()
-
-
-class GetChargeState(XmlCommandWithMessageHandling, ChargeStateParser):
+class GetChargeState(XmlCommandWithMessageHandling, ChargeState):
     """GetChargeState command."""
 
     NAME = "GetChargeState"
