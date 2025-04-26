3db7c79501977902ba788f9d246408db2d5f04a1
Giovanni Condello <condellog@gmail.com>
2025-04-26 11:49:47 +0200

    Handle XML ChargeState messages

--- deebot_client/messages/xml/__init__.py
+++ deebot_client/messages/xml/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from deebot_client.messages.xml.battery import BatteryInfo
+from deebot_client.messages.xml.charge import ChargeState
 from deebot_client.messages.xml.pos import Pos
 
 if TYPE_CHECKING:
@@ -12,11 +13,14 @@ if TYPE_CHECKING:
 
     from deebot_client.message import Message
 
-__all__: Sequence[str] = ["BatteryInfo", "Pos"]
+__all__: Sequence[str] = ["BatteryInfo", "ChargeState", "Pos"]
 # fmt: off
 # ordered by file asc
 _MESSAGES: list[type[Message]] = [
     BatteryInfo,
+
+    ChargeState,
+
     Pos
 ]
 # fmt: on
