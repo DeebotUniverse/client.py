2f8f34e0732cd00ead12aa52ebb6318775077a08
gpongelli <gpongelli@users.noreply.github.com>
2025-04-28 15:36:25 +0200

    add test_onStats
    test data comes from logs on gh issue; other fields are filtered out
    for now to not change StatsEvent dataclass.

--- tests/messages/json/test_stats.py
+++ tests/messages/json/test_stats.py
@@ -4,8 +4,8 @@ from typing import Any
 
 import pytest
 
-from deebot_client.events import CleanJobStatus, ReportStatsEvent
-from deebot_client.messages.json import ReportStats
+from deebot_client.events import CleanJobStatus, ReportStatsEvent, StatsEvent
+from deebot_client.messages.json import OnStats, ReportStats
 from tests.messages import assert_message
 
 
@@ -61,3 +61,63 @@ def test_ReportStats(data: dict[str, Any], expected: ReportStatsEvent) -> None:
     }
 
     assert_message(ReportStats, data, expected)
+
+
+@pytest.mark.parametrize(
+    ("data", "expected"),
+    [
+        (
+            {
+                "area":2,
+                "time":89,
+                "cid":"2002066096",
+                "start":"1744009746",
+                "type":"auto",
+                "enablePowerMop":1,
+                "powerMopType":2,
+                "aiopen":1,
+                "aitypes":[9],
+                "avoidCount":1
+            },
+            StatsEvent(
+                area=2,
+                time=89,
+                type="auto",
+            ),
+        ),
+        (
+            {
+                "area": 50,
+                "time": 56289,
+                "cid": "2002066096",
+                "start": "1744009746",
+                "type": "auto",
+                "enablePowerMop": 1,
+                "powerMopType": 2,
+                "aiopen": 1,
+                "aitypes": [9],
+                "avoidCount": 1
+            },
+
+            StatsEvent(
+                area=50,
+                time=56289,
+                type="auto",
+            ),
+        ),
+    ],
+)
+def test_onStats(data: dict[str, Any], expected: StatsEvent) -> None:
+    data = {
+        "header": {
+            "pri": 1,
+            "tzm": 480,
+            "ts": "1662017348913",
+            "ver": "0.0.1",
+            "fwVer": "1.8.2",
+            "hwVer": "0.1.1",
+        },
+        "body": {"data": data},
+    }
+
+    assert_message(OnStats, data, expected)
