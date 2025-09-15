DeebotUniverse/client.py

commit 29364e0300c87560febdde2bb2f6297ba144de93

Log device class on some command log entries (#1103)

deebot_client/command.py
@@ -109,8 +109,9 @@ class Command(ABC):
 
         except Exception:
             _LOGGER.warning(
-                "Could not execute command %s",
+                "Could not execute command %s for %s",
                 self.NAME,
+                device_info["class"],
                 exc_info=True,
             )
         return DeviceCommandResult(device_reached=False)
@@ -126,15 +127,19 @@ class Command(ABC):
             response = await self._execute_api_request(authenticator, device_info)
         except ApiTimeoutError:
             _LOGGER.warning(
-                "Could not execute command %s: Timeout reached",
+                "Could not execute command %s for %s: Timeout reached",
                 self.NAME,
+                device_info["class"],
             )
             return CommandResult(HandlingState.ERROR), {}
 
         result = self.__handle_response(event_bus, response)
         if result.state == HandlingState.ANALYSE:
             _LOGGER.debug(
-                "ANALYSE: Could not handle command: %s with %s", self.NAME, response
+                "ANALYSE: Could not handle command %s for %s: %s",
+                self.NAME,
+                device_info["class"],
+                response,
             )
             return (
                 CommandResult(
@@ -145,7 +150,12 @@ class Command(ABC):
                 response,
             )
         if result.state == HandlingState.ERROR:
-            _LOGGER.warning("Could not parse %s: %s", self.NAME, response)
+            _LOGGER.warning(
+                "Could not parse %s for %s: %s",
+                self.NAME,
+                device_info["class"],
+                response,
+            )
         return result, response
 
     async def _execute_api_request(
@@ -190,7 +200,7 @@ class Command(ABC):
             result = self._handle_response(event_bus, response)
         except Exception:
             _LOGGER.warning(
-                "Could not parse response for %s: %s",
+                "Could not parse response %s for %s: %s",
                 self.NAME,
                 response,
                 exc_info=True,

commit 8e0154bb95645dcd1e2fc3c6fb490a4055dee639

Fix bugs introduced with #1103

deebot_client/command.py
@@ -200,7 +200,7 @@ class Command(ABC):
             result = self._handle_response(event_bus, response)
         except Exception:
             _LOGGER.warning(
-                "Could not parse response %s for %s: %s",
+                "Could not parse response for %s: %s",
                 self.NAME,
                 response,
                 exc_info=True,

tests/test_command.py
@@ -111,5 +111,5 @@ async def test_execute_api_timeout_error(
     assert (
         "deebot_client.command",
         logging.WARNING,
-        "Could not execute command TestCommand: Timeout reached",
+        "Could not execute command TestCommand for get_class: Timeout reached",
     ) in caplog.record_tuples