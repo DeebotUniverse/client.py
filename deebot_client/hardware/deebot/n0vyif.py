c0a7e9787687e8911269e9d8ec708a576499846b
Sanji78 <166992508+Sanji78@users.noreply.github.com>
2025-09-17 13:57:18 +0200

    Support for Deebot X8 PRO OMNI

--- deebot_client/hardware/deebot/n0vyif.py
+++ deebot_client/hardware/deebot/n0vyif.py
@@ -34,10 +34,7 @@ from deebot_client.commands.json.carpet import (
 from deebot_client.commands.json.charge import Charge
 from deebot_client.commands.json.charge_state import GetChargeState
 from deebot_client.commands.json.child_lock import GetChildLock, SetChildLock
-from deebot_client.commands.json.clean import (
-    CleanAreaV2,
-    CleanV2
-)
+from deebot_client.commands.json.clean import CleanAreaV2, CleanV2
 from deebot_client.commands.json.clean_count import GetCleanCount, SetCleanCount
 from deebot_client.commands.json.clean_logs import GetCleanLogs
 from deebot_client.commands.json.clean_preference import (
