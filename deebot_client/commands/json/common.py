c96f351dc927c313245d98bdb9f6e4a07b93fd37
Robert Resch <robert@resch.dev>
2025-10-09 22:48:51 +0000

    Use time.time()

--- deebot_client/commands/json/common.py
+++ deebot_client/commands/json/common.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from datetime import UTC, datetime
+import time
 from types import MappingProxyType
 from typing import TYPE_CHECKING, Any
 
@@ -45,7 +45,7 @@ class JsonCommand(Command, ABC):
         payload = {
             "header": {
                 "pri": "1",
-                "ts": datetime.now(tz=UTC).timestamp(),
+                "ts": time.time(),
                 "tzm": 480,
                 "ver": "0.0.50",
             }
