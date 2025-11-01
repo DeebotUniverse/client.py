03e8eebdca16e0ef7c70c6389cdef65b1152238f
ha-newbie <143764064+ha-newbie@users.noreply.github.com>
2025-11-01 10:32:31 +0900

    fix code

--- deebot_client/commands/json/auto_empty.py
+++ deebot_client/commands/json/auto_empty.py
@@ -28,10 +28,9 @@ class SetAutoEmpty(ExecuteCommand):
         if frequency is not None and not isinstance(frequency, Frequency):
             frequency = get_enum(Frequency, frequency)
 
-        if type(enable) is not bool and frequency is None:
-            frequency = enable
-            frequency = get_enum(Frequency, frequency)
-            enable = false if frequency == "manual" else true
+        if type(enable) is str and frequency is None:
+            frequency = get_enum(Frequency, enable)
+            enable = False if frequency == "manual" else True
 
         params: dict[str, Any] = {}
         if enable is not None:
