2a95cef93e8776ae78db45ba5b930ff6de36208d
Nicolas <205067365+N1KHL@users.noreply.github.com>
2026-05-22 20:00:49 +0200

    refactor: optimize CleanAreaV2 for freeClean and remove redundant class

--- deebot_client/commands/json/__init__.py
+++ deebot_client/commands/json/__init__.py
@@ -13,7 +13,7 @@ from .carpet import GetCarpetAutoFanBoost, SetCarpetAutoFanBoost
 from .charge import Charge
 from .charge_state import GetChargeState
 from .child_lock import GetChildLock, SetChildLock
-from .clean import Clean, CleanArea, CleanAreaFreeClean, CleanV2, GetCleanInfo, GetCleanInfoV2
+from .clean import Clean, CleanArea, CleanV2, GetCleanInfo, GetCleanInfoV2
 from .clean_count import GetCleanCount, SetCleanCount
 from .clean_logs import GetCleanLogs
 from .clean_preference import GetCleanPreference, SetCleanPreference
@@ -62,7 +62,6 @@ __all__ = [
     "Charge",
     "Clean",
     "CleanArea",
-    "CleanAreaFreeClean",
     "CleanV2",
     "ClearMap",
     "GetAdvancedMode",
@@ -175,7 +174,6 @@ _COMMANDS: list[type[JsonCommand]] = [
     Clean,
     CleanV2,
     CleanArea,
-    CleanAreaFreeClean,
     GetCleanInfo,
     GetCleanInfoV2,
 
--- tests/commands/json/test_clean.py
+++ tests/commands/json/test_clean.py
@@ -9,7 +9,6 @@ from deebot_client.commands.json import GetCleanInfo
 from deebot_client.commands.json.clean import (
     Clean,
     CleanArea,
-    CleanAreaFreeClean,
     CleanAreaV2,
     CleanV2,
     GetCleanInfoV2,
@@ -138,14 +137,14 @@ async def test_Clean_act(
             },
         ),
         (
-            CleanAreaFreeClean(CleanMode.FREE_CLEAN, [5, 8]),
+            CleanAreaV2(CleanMode.FREE_CLEAN, [5, 8]),
             {
                 "act": "start",
                 "content": {"type": "freeClean", "value": "1,5,8"},
             },
         ),
         (
-            CleanAreaFreeClean(CleanMode.FREE_CLEAN, [0], cleanings=2),
+            CleanAreaV2(CleanMode.FREE_CLEAN, [0], cleanings=2),
             {
                 "act": "start",
                 "content": {"type": "freeClean", "value": "2,0"},
@@ -162,7 +161,7 @@ async def test_Clean_act(
     ],
 )
 async def test_CleanArea(
-    command: CleanArea | CleanAreaV2 | CleanAreaFreeClean,
+    command: CleanArea | CleanAreaV2,
     args: dict[str, str],
 ) -> None:
     await assert_execute_command(command, args)
