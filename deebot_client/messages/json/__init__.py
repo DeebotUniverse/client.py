68eb178ede8fd533b37bcff000b0ba4b1b476014
monsivar <32893134+monsivar@users.noreply.github.com>
2026-06-02 12:34:33 +0200

    Add bd_setting to module exports
    feat(messages): register bd_setting parser

--- deebot_client/messages/json/__init__.py
+++ deebot_client/messages/json/__init__.py
@@ -14,6 +14,7 @@ from .map import OnCachedMapInfo, OnMajorMap, OnMapInfoV2, OnMapSetV2
 from .station_state import OnStationState
 from .stats import OnStats, ReportStats
 from .work_state import OnWorkState
+from . import bd_setting
 
 _LOGGER = get_logger(__name__)
 
@@ -27,6 +28,7 @@ __all__ = [
     "OnStats",
     "OnWorkState",
     "ReportStats",
+    "bd_setting",
 ]
 
 # fmt: off
