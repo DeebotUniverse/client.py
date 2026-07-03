8804e57d0d8a2b2e12eb04e869c4f12d13020289
monsivar <32893134+monsivar@users.noreply.github.com>
2026-07-03 17:55:29 +0200

    Add SetAreaParameter to _COMMANDS list

--- deebot_client/commands/json/__init__.py
+++ deebot_client/commands/json/__init__.py
@@ -143,6 +143,7 @@ __all__ = [
 _COMMANDS: list[type[JsonCommand]] = [
     GetAdvancedMode,
     SetAdvancedMode,
+    SetAreaParameter,
 
     auto_empty.GetAutoEmpty,
     auto_empty.SetAutoEmpty,
