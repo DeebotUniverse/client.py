1c676e5ecb9bd4d622c93e0099bc8135f650115b
monsivar <32893134+monsivar@users.noreply.github.com>
2026-07-03 17:38:49 +0200

    Remove SetAreaParameter from messages module
    Removed 'SetAreaParameter' from the exported symbols.

--- deebot_client/messages/json/__init__.py
+++ deebot_client/messages/json/__init__.py
@@ -14,7 +14,6 @@ from .map import OnCachedMapInfo, OnMajorMap, OnMapInfoV2, OnMapSetV2
 from .station_state import OnStationState
 from .stats import OnStats, ReportStats
 from .work_state import OnWorkState
-from .area_parameter import SetAreaParameter
 
 _LOGGER = get_logger(__name__)
 
@@ -28,7 +27,6 @@ __all__ = [
     "OnStats",
     "OnWorkState",
     "ReportStats",
-    "SetAreaParameter",
 ]
 
 # fmt: off
