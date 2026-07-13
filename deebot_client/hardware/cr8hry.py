c6f21ae6574944714c07bd042bb939475bddc23f
zhyf0610 <42296671+zhyf0610@users.noreply.github.com>
2026-07-13 19:45:35 +0800

    Update cr8hry.py

--- deebot_client/hardware/cr8hry.py
+++ deebot_client/hardware/cr8hry.py
@@ -92,6 +92,7 @@ from deebot_client.events import (
     MajorMapEvent,
     MapChangedEvent,
     MapTraceEvent,
+    MopAutoWashFrequencyEvent,
     MultimapStateEvent,
     NetworkInfoEvent,
     OtaEvent,
@@ -111,8 +112,6 @@ from deebot_client.events import (
     auto_empty,
     water_info,
 )
-from deebot_client.events.auto_empty import AutoEmptyEvent
-from deebot_client.events.mop_auto_wash_frequency import MopAutoWashFrequencyEvent
 from deebot_client.models import StaticDeviceInfo
 
 
