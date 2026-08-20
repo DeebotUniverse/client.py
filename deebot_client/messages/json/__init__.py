3cb4391040649843227d059fffd5d06030f561e9
monsivar <32893134+monsivar@users.noreply.github.com>
2026-08-20 08:58:08 +0200

    Update __init__.py
    Fix message import ordering

--- deebot_client/messages/json/__init__.py
+++ deebot_client/messages/json/__init__.py
@@ -6,9 +6,8 @@ import re
 
 from deebot_client.logging_filter import get_logger
 from deebot_client.message import Message
-
-from .auto_empty import OnAutoEmpty
 from .area_parameter import OnAreaParameter
+from .auto_empty import OnAutoEmpty
 from .battery import OnBattery
 from .gps_position import OnGpsPos
 from .map import OnCachedMapInfo, OnMajorMap, OnMapInfoV2, OnMapSetV2
